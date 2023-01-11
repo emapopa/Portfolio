db15e76cc42c0bf03102c594a752ef438d4cf6d8
emapopa <emapopa1995@gmail>
2023-01-11 23:11:00 +0100

    fixed typewriter efec

--- main.js
+++ main.js
@@ -13,14 +13,41 @@ hamburger.addEventListener('click', ()=>{
     hamburger.classList.toggle("toggle");
 });
 
-var i = 0;
-var txt = 'Lorem ipsum typing hvbjhv gkhbk effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typewriter").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+// set up text to print, each item in array is new line
+let aText = new Array(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. ", 
+    "Quia, asperiores maxime aut aliquid architecto reiciendis accusamus v"
+    );
+    let iSpeed = 100; // time delay of print out
+    let iIndex = 0; // start printing array at this posision
+    let iArrLength = aText[0].length; // the length of the text array
+    let iScrollAt = 20; // start scrolling up at this many lines
+     
+    let iTextPos = 0; // initialise text position
+    let sContents = ''; // initialise contents variable
+    let iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
